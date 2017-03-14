5ee9f10507497817b66cca1c8ac4b40c8eff7704
Bill Langley <brainstormwilly@gmail.com>
2017-03-13 21:44:06 -0700

    Installed firebase, ui-router

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+(function(){
+  function config($locationProvider, $stateProvider){
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+    // $stateProvider
+    //   .state('landing',{
+    //     url: '/',
+    //     controller: 'LandingCtrl as landing',
+    //     templateUrl: '/templates/landing.html'
+    //   });
+  }
+  angular
+    .module('blocJams', ['ui.router', 'firebase'])
+    .config(config);
+
+})();
